ee991c5b2f73790267bf5e7c36aa856d4423ca60
casey gerstle <gerstle@gmail.com>
2025-05-19 18:18:05 -0700

    update user model view for admin, rename to sandtrack, etc.

--- src/admin/UserModelView.py
+++ src/admin/UserModelView.py
@@ -4,4 +4,4 @@ from src import AuthenticatedModelView
 class UserModelView(AuthenticatedModelView):
     """Waypoint model admin view"""
     form_excluded_columns = ("password",)
-    list_columns = ("name", "email")
+    column_list = ("name", "email")
